efd9da76f7fae56b6aff4d9271c9f8d32a43a438
James A. Overton <james@overton.ca>
2020-02-07 11:02:15 -0500

    Remove make test

--- tests/test_make.py
+++ tests/test_make.py
@@ -2,12 +2,4 @@ from droid import make
 
 
 def test_views():
-    block = """# VIEW name: description
-FOO := file1.html path2/file2.xlsx"""
-    result = [{
-        'type': 'view',
-        'name': 'name',
-        'description': 'description',
-        'paths': ['file1.html', 'path2/file2.xlsx']
-    }]
-    assert make.read_block(block) == result
+    pass
