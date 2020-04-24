83d3f3280f28d7995697c5a808fc9aa68b1c2048
Michael Cuffaro <mike@gwydion>
2020-04-24 13:27:58 -0400

    (1) move the console result block above the output block, and repeat it again after the output block if the console has more than fifty lines in it; (2) when prompting to kill an action in the case where another action has been requested, move the prompt to the console status area instead of to its own page.

--- src/droid/html.clj
+++ src/droid/html.clj
@@ -305,46 +305,80 @@
 (defn hit-branch!
   "Render a branch, possibly performing an action on it in the process. Note that this function will
   call the branch's agent to potentially modify the branch."
-  [{{:keys [project-name branch-name action force-kill]} :params
+  [{{:keys [project-name branch-name action confirm-kill force-kill]} :params
     :as request}]
-  (letfn [(render-branch-console
-            [{:keys [action start-time run-time command console exit-code cancelled?]
-              :as branch}]
+  (letfn [(render-action-status
+            [{:keys [run-time exit-code cancelled?] :as branch}]
+
+            (cond
+              cancelled?
+              [:p {:class "alert alert-warning"} "Process cancelled"]
+
+              (nil? exit-code)
+              [:div]
+
+              (not (realized? exit-code))
+              [:p {:class "alert alert-warning"}
+               (str "Processes still running after "
+                    (-> run-time (/ 1000) (float) (Math/ceil) (int)) " seconds.")
+               [:span {:class "col-sm-2"} [:a {:class "btn btn-success btn-sm"
+                                               :href (str "/" project-name "/branches/"
+                                                          branch-name)}
+                                           "Refresh"]]
+               [:span {:class "col-sm-2"} [:a {:class "btn btn-danger btn-sm"
+                                               :href "?action=cancel-DROID-process"} "Cancel"]]]
+
+              (= 0 @exit-code)
+              [:p {:class "alert alert-success"} "Success"]
+
+              :else
+              [:p {:class "alert alert-danger"} (str "ERROR: Exit code " @exit-code)]))
+
+          (prompt-to-kill
+            [{:keys [start-time] :as branch}]
+            (let [readable-start (->> start-time (java.time.Instant/ofEpochMilli) (str))
+                  prev-action (:action branch)]
+              [:div {:class "alert alert-warning"}
+               [:p [:span {:class "text-monospace font-weight-bold"} prev-action]
+                " is still not complete. You must cancel it before running "
+                [:span {:class "text-monospace font-weight-bold"} action]
+                ". Do you really want to do this?"]
+               [:span [:a {:class "btn btn-secondary btn-sm"
+                           :href (str "/" project-name "/branches/" branch-name)}
+                       "No, do nothing"]]
+               [:span {:class "col-sm-1"}]
+               [:span [:a {:class "btn btn-danger btn-sm"
+                           :href (str "/" project-name "/branches/" branch-name
+                                      "?action=" action "&force-kill=1")}
+                       "Yes, go ahead"]]]))
+
+          (render-console
+            [{:keys [action start-time command console] :as branch}]
             ;; Render the part of the branch corresponding to the console:
-            [:div
+            [:div {:id "console"}
              (let [readable-start (->> start-time (java.time.Instant/ofEpochMilli) (str))]
                [:p {:class "alert alert-info"} (str "Action " action " started at ")
                 [:span {:class "date"} readable-start] " ("
                 [:span {:class "since"} readable-start] ")"])
 
+             ;; The status bar and refresh and cancel buttons:
+             (if-not (nil? confirm-kill)
+               (prompt-to-kill branch)
+               (render-action-status branch))
+
+             ;; The command and console output:
              [:pre [:code {:class "shell"} (str "$ " command)]]
              [:pre [:code {:class "shell"} console]]
 
-             (cond
-               cancelled?
-               [:p {:class "alert alert-warning"} "Process cancelled"]
+             ;; If there are more than fifty lines in the console, render the buttons again:
+             (when (<= 50 (->> console
+                               (filter #(= \newline  %))
+                               (count)))
+               (if-not (nil? confirm-kill)
+                 (prompt-to-kill branch)
+                 (render-action-status branch)))])
 
-               (nil? exit-code)
-               [:div]
-
-               (not (realized? exit-code))
-               [:p {:class "alert alert-warning"}
-                (str "Processes still running after "
-                     (-> run-time (/ 1000) (float) (Math/ceil) (int)) " seconds.")
-                [:span {:class "col-sm-2"} [:a {:class "btn btn-success btn-sm"
-                                                :href (str "/" project-name "/branches/"
-                                                           branch-name)}
-                                            "Refresh"]]
-                [:span {:class "col-sm-2"} [:a {:class "btn btn-danger btn-sm"
-                                                :href "?action=cancel"} "Cancel"]]]
-
-               (= 0 @exit-code)
-               [:p {:class "alert alert-success"} "Success"]
-
-               :else
-               [:p {:class "alert alert-danger"} (str "ERROR: Exit code " @exit-code)])])
-
-          (render-branch-outer-page [{:keys [branch-name Makefile process] :as branch}]
+          (render-branch-page [{:keys [branch-name Makefile process] :as branch}]
             ;; Render the page for a branch:
             (html-response
              request
@@ -366,7 +400,7 @@
                          [:h3 "Console"]
                          (if (nil? process)
                            [:p "Press a button above to execute an action."]
-                           (render-branch-console branch))]]}))
+                           (render-console branch))]]}))
 
           (launch-process [branch]
             (let [process (sh/proc "bash" "-c"
@@ -387,89 +421,71 @@
                      :process process
                      :start-time (System/currentTimeMillis)
                      :cancelled? false
-                     :exit-code (future (sh/exit-code process)))))
-
-          (act-on-branch [branch]
-            (let [last-exit-code (->> branch :exit-code)]
-              ;; Perform the requested action:
-              (cond
-                (= action "cancel")
-                (if (and (not (nil? last-exit-code))
-                         (not (realized? last-exit-code)))
-                  (kill-process branch)
-                  branch)
-
-                ;; If the specified action is recognized then launch a corresponding process, killing
-                ;; any other running process first:
-                (some #(= % action) (->> branch :Makefile :general-actions))
-                (do
-                  (if (and (not (nil? last-exit-code))
-                           (not (realized? last-exit-code)))
-                    (->> branch (kill-process) (launch-process))
-                    (->> branch (launch-process))))
-
-                :else
-                (do
-                  (when (not (nil? action))
-                    (log/warn "Unrecognized action:" action))
-                  branch))))
-
-          (prompt-to-kill [{:keys [start-time run-time command console] :as branch}]
-            ;; Only one user process per branch should run. If there is one already running, ask the
-            ;; user whether he would like to kill it:
-            (let [readable-start (->> start-time (java.time.Instant/ofEpochMilli) (str))]
-              (html-response
-               request
-               {:content
-                [:div
-                 [:div {:class "alert alert-info"}
-                  "A process, begun at "
-                  [:span {:class "date"} readable-start] " ("
-                  [:span {:class "since"} readable-start] "), "
-                  "is already in progress. You must cancel it before running action "
-                  [:span {:class "text-monospace font-weight-bold"} action] "."]
-                 [:div
-                  [:span [:a {:class "btn btn-secondary btn-sm"
-                              :href (str "/" project-name "/branches/" branch-name)}
-                          "Go back to " branch-name]]
-                  [:span {:class "col-sm-1"}]
-                  [:span [:a {:class "btn btn-danger btn-sm"
-                              :href (str "/" project-name "/branches/" branch-name
-                                         "?action=" action "&force-kill=1")}
-                          "Cancel process and run action"]]
-                  [:span {:class "col-sm-1"}]]
-                 [:hr {:class "line1"}]
-                 [:div
-                  [:h3 "Console"]
-                  [:pre [:code {:class "shell"} (str "$ " command)]]
-                  [:pre [:code {:class "shell"} console]]]]})))]
+                     :exit-code (future (sh/exit-code process)))))]
 
     (let [branch-key (keyword branch-name)
           project-key (keyword project-name)
           branch-agent (->> data/branches project-key branch-key)
           last-exit-code (:exit-code @branch-agent)]
+
+      ;; Send off a branch refresh and then await for it (and any previous jobs) to finish:
       (send-off branch-agent data/refresh-branch)
       (await branch-agent)
 
-      ;; If there is an action (other than cancel) and the force-kill parameter is not present, and
-      ;; if there is a process associated with the branch that is still running, then ask the user
-      ;; whether to kill it or not.
-      (if (and (not (nil? action))
-               (not (= action "cancel"))
-               (nil? force-kill)
-               (not (nil? last-exit-code))
-               (not (realized? last-exit-code)))
-        (prompt-to-kill @branch-agent)
-        ;; Otherwise just continue on as normal:
-        (do
-          ;; Perform any needed actions on the branch and wait for it to be done:
-          (send-off branch-agent act-on-branch)
-          (await branch-agent)
+      ;; Note that below, whenever an action is performed on a branch, we then redirect back to
+      ;; the branch page. The main reason for this is to get rid of the '?action=...' part of the
+      ;; URL in the browser's address bar. This is important because otherwise the user could then
+      ;; re-launch the action by mistake simply by hitting her browser's refresh button.
 
-          ;; If we performed an action, then we now redirect to the branch page (the main reason for
-          ;; this is to get rid of the '?action=...' part of the URL in the address bar, which is
-          ;; desirable because we don't want to kick off the action again just because the user hits
-          ;; her browser's refresh button):
-          (if-not (nil? action)
-            (redirect (str "/" project-name "/branches/" branch-name))
-            (render-branch-outer-page @branch-agent)))))))
+      ;; Note also that below we do not call (await) after calling (send-off), because below we
+      ;; always redirect back to the branch page, which results in another call to this function,
+      ;; which always calls await when it starts (see above).
+      (cond
+        ;; If this is a cancel action, kill the existing project and redirect to the branch page.
+        (= action "cancel-DROID-process")
+        (do
+          (send-off branch-agent kill-process)
+          (redirect (str "/" project-name "/branches/" branch-name)))
+
+        ;; If there is no action to be performed, or the action is unrecognised, then just render
+        ;; the page using the existing branch data:
+        (not-any? #(= % action) (->> @branch-agent :Makefile :general-actions))
+        (do
+          (when-not (nil? action)
+            (log/warn "Unrecognized action:" action))
+          (render-branch-page @branch-agent))
+
+        ;; If there is a currently running process (indicated by the presence of an unrealised
+        ;; exit code on the branch), then what needs to be done will be determined by the
+        ;; presence of further query parameters sent in the request.
+        (and (not (nil? last-exit-code))
+             (not (realized? last-exit-code)))
+        (cond
+          ;; If the force-kill parameter has been sent, then simply kill the old process and
+          ;; relaunch the new one:
+          (not (nil? force-kill))
+          (do
+            (send-off branch-agent kill-process)
+            (send-off branch-agent launch-process)
+            (redirect (str "/" project-name "/branches/" branch-name)))
+
+          ;; If the confirm-kill parameter has been sent, then simply render the page for the
+          ;; branch. The confirm-kill flag will be recognised during rendering and a prompt
+          ;; will be displayed to request that the user confirm his action:
+          (not (nil? confirm-kill))
+          (render-branch-page @branch-agent)
+
+          ;; If neither the force-kill nor the confirm-kill parameter has been sent, then
+          ;; send the action again (by redirecting to the branch page with the action parameter set)
+          ;; and include the confirm-kill parameter which will be used during rendering to display
+          ;; a prompt.
+          :else
+          (redirect (str "/" project-name "/branches/" branch-name "?action=" action
+                         "&confirm-kill=1#console")))
+
+        ;; If there is an action to be performed and there is no currently running process, then
+        ;; simply launch the new process:
+        :else
+        (do
+          (send-off branch-agent launch-process)
+          (redirect (str "/" project-name "/branches/" branch-name)))))))
