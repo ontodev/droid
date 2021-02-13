01f43cb9202ea592ab14718ba7295e6f9d63ccb2
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-02-12 20:14:24 -0500

    add back line of code that did not need to be removed

--- src/droid/html.clj
+++ src/droid/html.clj
@@ -1734,7 +1734,9 @@
         ;; Serves the view from the filesystem:
         deliver-file-view #(-> (get-make-dir project-name branch-name)
                                (str "/" decoded-view-path)
-                               (file-response))
+                               (file-response)
+                               ;; Views must not be cached by the client browser:
+                               (assoc :headers {"Cache-Control" "no-store"}))
 
         ;; Used for checking whether the requested view is in the workspace directory:
         canonical-ws-dir (-> (get-workspace-dir project-name branch-name) (io/file)
